888da8104765deaf4ea3425f960cecd1729ae5c3
Fabrice <f.nkurunziz@alustudent.com>
2024-04-15 11:38:31 +0200

    created the User Schema

---
+++ Backend/models/user.mjs
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userShema);
+
+export default User;

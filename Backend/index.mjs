64f615ed7000df43fa7b8e7ca2b01c5bda9839b5
Fabrice <f.nkurunziz@alustudent.com>
2024-04-15 09:53:11 +0200

    created different folders for code maintainance

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -1,6 +1,12 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(`Hello from the frontend`);
+  res.status(200).end("Hello from the server");
+});
 
 const PORT = process.env.PORT || 5000;
 

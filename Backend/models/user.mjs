d23d75f2dc91e93f72b27fdb0ede97d0e9989b73
Fabrice <f.nkurunziz@alustudent.com>
2024-04-25 11:11:07 +0200

    Updated the userModel, now added the isAdmin field with a default value of false

--- Backend/models/user.mjs
+++ Backend/models/user.mjs
@@ -20,6 +20,10 @@ const userShema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );

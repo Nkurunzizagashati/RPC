c6d2333759a6318fb9283cb5b030d2a40a96f267
Fabrice <f.nkurunziz@alustudent.com>
2024-04-29 11:15:50 +0200

    now added the courseModules field in the courseSchema

--- Backend/models/course.mjs
+++ Backend/models/course.mjs
@@ -14,6 +14,22 @@ const courseSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    courseModules: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        introduction: {
+          type: String,
+          required: true,
+        },
+        body: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
   { timestamps: true }
 );

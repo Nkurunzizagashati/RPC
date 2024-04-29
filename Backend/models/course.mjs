9707a71972b9e00387456975cc829506d891a075
Fabrice <f.nkurunziz@alustudent.com>
2024-04-29 10:57:16 +0200

    created the courseSchema in the course.mjs in the models folder and added title and description fields

---
+++ Backend/models/course.mjs
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);

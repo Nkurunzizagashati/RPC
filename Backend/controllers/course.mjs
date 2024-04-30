f61399f681174b78bc144903f5b1f78aa979dd30
Fabrice <f.nkurunziz@alustudent.com>
2024-04-30 10:31:11 +0200

    created the createCourseController

---
+++ Backend/controllers/course.mjs
@@ -0,0 +1,20 @@
+import Course from "../models/course.mjs";
+import { validationResult, matchedData } from "express-validator";
+
+const createCourseController = async (req, res) => {
+  const result = validationResult(req);
+
+  if (!result.notEmpty())
+    return res.status(401).json({ err: result.errors[0].msg });
+
+  try {
+    const data = matchedData(req);
+    const newCourse = await new Course(data);
+    if (!newCourse) return res.json({ err: "Something went wrong" });
+    res.status(201).json({ msg: "Course created successfully!", newCourse });
+  } catch (error) {
+    return res.status(401).json({ err: error.message });
+  }
+};
+
+export { createCourseController };

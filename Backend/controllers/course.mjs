22853fb92323ebf0de07b60b4fbcfc5711132878
Fabrice <f.nkurunziz@alustudent.com>
2024-04-30 11:51:08 +0200

    created the controller for getting all the courses

--- Backend/controllers/course.mjs
+++ Backend/controllers/course.mjs
@@ -1,13 +1,28 @@
 import Course from "../models/course.mjs";
+import jwt from "jsonwebtoken";
 import { validationResult, matchedData } from "express-validator";
+import User from "../models/user.mjs";
+
+const jwt_secret = process.env.JWT_SECRET;
 
 const createCourseController = async (req, res) => {
-  const result = validationResult(req);
-
-  if (!result.isEmpty())
-    return res.status(401).json({ err: result.errors[0].msg });
-
   try {
+    const result = validationResult(req);
+
+    const token = req.cookies.token;
+    if (!token) return res.status(403).json({ err: "You need to login first" });
+
+    const loggedInUserId = jwt.decode(token, jwt_secret).id;
+    const loggedInUser = await User.findById(loggedInUserId);
+
+    if (!loggedInUser.isAdmin)
+      return res
+        .status(403)
+        .json({ err: "You don't have permission to create a course" });
+
+    if (!result.isEmpty())
+      return res.status(401).json({ err: result.errors[0].msg });
+
     const data = matchedData(req);
     const newCourse = await new Course(data);
     if (!newCourse) return res.json({ err: "Something went wrong" });
@@ -17,7 +32,21 @@ const createCourseController = async (req, res) => {
   }
 };
 
-const getAllCoursesController = (req, res) => {};
+const getAllCoursesController = async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token)
+      return res.status(403).json({ err: "You need to loggin first" });
+    const loggedInUserId = jwt.decode(token, jwt_secret).id;
+    const loggedInUser = await User.findById(loggedInUserId);
+    if (!loggedInUser)
+      return res.status(403).json({
+        err: "User not recognized, you need to loggin or register to access the courses",
+      });
+  } catch (error) {
+    res.json({ err: error.message });
+  }
+};
 
 const updateCourseController = (req, res) => {};
 

d38dae01ec56c38359fb0482105bf31dea9b52f2
Fabrice <f.nkurunziz@alustudent.com>
2024-04-30 10:31:53 +0200

    used the createCourseValidator middleware in the course route

--- Backend/routes/course.mjs
+++ Backend/routes/course.mjs
@@ -1,10 +1,13 @@
 import express from "express";
+import { checkSchema } from "express-validator";
+import { createCourseValidator } from "../middlewares/coursesValidationSchemas.mjs";
+import { createCourseController } from "../controllers/course.mjs";
 
 const router = express.Router();
 
-router.get("/", getAllCoursesController);
-router.post("/", createCourseController);
-router.patch("/:id", updateCourseController);
-router.delete("/:id", deleteCourseController);
+// router.get("/", getAllCoursesController);
+router.post("/", checkSchema(createCourseValidator), createCourseController);
+// router.patch("/:id", updateCourseController);
+// router.delete("/:id", deleteCourseController);
 
 export default router;
